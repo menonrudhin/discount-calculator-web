8f31c54d0d7639d0dbfc4e79805a572525b9d753
Rudhin <rudhin@localhost.localdomain>
2020-12-04 23:32:49 -0500

    PoC completed, now include add/remove button to add remove rows as per user's wish

--- discount-calculator-web/src/app/model/row-model.ts
+++ discount-calculator-web/src/app/model/row-model.ts
@@ -5,33 +5,4 @@ export class RowModel {
         this.txtDiscount='0';
         this.txtAmount='0';        
     }
-
-    // public get txtPrice(){
-    //     return this._txtPrice;
-    // }
-
-    // public set txtPrice(txtPrice:string){
-    //     this._txtPrice=txtPrice;
-    // }
-
-    // public get txtDiscount(){
-    //     return this._txtDiscount;
-    // }
-
-    // public set txtDiscount(txtDiscount:string){
-    //     this._txtDiscount=txtDiscount;
-    // }
-
-    // public get txtAmount(){
-    //     return this._txtAmount;
-    // }
-
-    // public set txtAmount(txtAmount:string){
-    //     this._txtAmount=txtAmount;
-    // }
-
-    // public calculate() : boolean {        
-    //     this._txtAmount = '100';
-    //     return true;
-    // }
 }
--- discount-calculator-web/src/app/sheet/sheet.component.ts
+++ discount-calculator-web/src/app/sheet/sheet.component.ts
@@ -8,6 +8,9 @@ import { RowModel } from '../model/row-model';
 })
 export class SheetComponent implements OnInit {
   rows:RowModel[] = [];
+  public txtYouSave:string;
+  public txtTax:string;
+  public txtTotal:string;
 
   constructor() { 
     this.rows = [{
@@ -23,29 +26,66 @@ export class SheetComponent implements OnInit {
       txtDiscount : '50',
       txtAmount : '50' 
     }];
+
+    this.txtYouSave='0';
+    this.txtTax='0';
+    this.txtTotal='0';
   }
 
   ngOnInit(): void {
+    this.txtYouSave='0';
+    this.txtTax='0';
+    this.txtTotal='0';
   }
 
   onTxtPriceChange(txtPrice:string,index:number){
     this.rows[index].txtPrice=txtPrice;
     console.log('set new price',this.rows[index].txtPrice,' , index = ', index);
-    this.calculate(this.rows[index]);
+    this.calculateRow(this.rows[index]);
+    this.calculateResult();
   }
 
   onTxtDiscountChange(txtDiscount:string,index:number){
     this.rows[index].txtDiscount=txtDiscount;
     console.log('set new discount',this.rows[index].txtDiscount,' , index = ', index);
-    this.calculate(this.rows[index]);
+    this.calculateRow(this.rows[index]);
+    this.calculateResult();
   }
 
-  calculate(rowModel : RowModel){
+  onTxtTaxChange(txtTax:string){
+    this.txtTax=txtTax;
+    console.log('set new Tax',this.txtTax);
+    this.calculateResult();
+  }
+
+  calculateRow(rowModel : RowModel){
     let price = Number(rowModel.txtPrice);
     let discount = Number(rowModel.txtDiscount);
     let amount = price - (price * (discount/100));
     console.log('new amount', amount);
-    rowModel.txtAmount = String(amount);
+    rowModel.txtAmount = String(amount);    
+  }
+
+  calculateResult(){
+    let price:number = 0;
+    let amount:number = 0;
+    let youSave:number = 0;
+    let total:number = 0;
+    let actualTotal:number = 0;
+    let tax:number = Number(this.txtTax);
+    this.rows.forEach(function (e) {
+      price = Number(e.txtPrice);
+      amount = Number(e.txtAmount);
+      
+      total+=amount;
+      actualTotal+=price;
+    });
+    total = total + (total*(tax/100));
+    actualTotal = actualTotal + (actualTotal * (tax/100));
+    youSave = actualTotal - total;
+    this.txtYouSave = youSave+'';
+    this.txtTotal = total+'';
+    console.log('calculate result , ',this.txtYouSave,' , ',this.txtTax,' , ',this.txtTotal);
   }
 
 }
