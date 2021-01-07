b98c20015b59de6e961617a9eae23fb4c13f8147
Rudhin <rudhin@localhost.localdomain>
2021-01-06 21:35:41 -0500

    calculate value in new currency with exchange rate
    next get exchange rate from API
    https://api.exchangeratesapi.io/latest?base=CAD&symbols=INR

--- discount-calculator-web/src/app/exchange-rate/exchange-rate.component.ts
+++ discount-calculator-web/src/app/exchange-rate/exchange-rate.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { getNumberOfCurrencyDigits } from '@angular/common';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { isNumericLiteral, NumericLiteral } from 'typescript';
 
 @Component({
   selector: 'app-exchange-rate',
   templateUrl: './exchange-rate.component.html',
   styleUrls: ['./exchange-rate.component.css']
 })
-export class ExchangeRateComponent implements OnInit {
+export class ExchangeRateComponent implements OnInit,OnChanges {
 
-  constructor() { }
+  fromValue : string;
+  toValue : string;
+  toCurrencyAmount : string;
+  @Input() fromCurrencyAmount : string;
+  exchangeRate : number;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.fromValue = 'CAD';
+    this.toValue = 'INR';
+    this.toCurrencyAmount = '0';
+    this.fromCurrencyAmount = '10';
+    this.exchangeRate = 57;
+
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  ngOnChanges() : void {
+    let fromVal = Number(this.fromCurrencyAmount);
+    let toVal = fromVal*this.exchangeRate;
+    console.log('converted = ',toVal);
+    this.toCurrencyAmount = toVal+'';
+  }
 }
