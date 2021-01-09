794189230ac1ef235f11efa9a8fc11bcf80a6186
Rudhin <rudhin@localhost.localdomain>
2021-01-08 21:56:49 -0500

    default selection of from and to currency

--- discount-calculator-web/src/app/exchange-rate/exchange-rate.component.ts
+++ discount-calculator-web/src/app/exchange-rate/exchange-rate.component.ts
@@ -3,6 +3,7 @@ import { HttpResponse } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { isNumericLiteral, NumericLiteral } from 'typescript';
 import { ExchangeratesService } from '../exchangerates.service';
+import { CurrencyModel } from '../model/currency-model';
 
 @Component({
   selector: 'app-exchange-rate',
@@ -16,12 +17,13 @@ export class ExchangeRateComponent implements OnInit,OnChanges {
   toCurrencyAmount : string;
   @Input() fromCurrencyAmount : string;
   exchangeRate : number;
+  currencies:CurrencyModel[];
 
   constructor(private exchangeRateService : ExchangeratesService) { 
     this.fromValue = 'CAD';
     this.toValue = 'INR';
     this.toCurrencyAmount = '0';
-    this.fromCurrencyAmount = '10';
+    this.fromCurrencyAmount = '0';
     this.exchangeRateService.getExchangeRate(this.fromValue,this.toValue).subscribe((res : any) => {    // fetch latest exchange rate from the API  
       let properties = Object.getOwnPropertyNames(res.rates);            
       let property= properties[0];      
@@ -29,7 +31,11 @@ export class ExchangeRateComponent implements OnInit,OnChanges {
       console.log(this.fromValue,' to ',this.toValue,' exchange rate fetched = ',this.exchangeRate);
     });
     this.exchangeRate = 0; // will be overriden by the exchange rate fetched from the API
-
+    this.currencies = [{symbol: 'CAD',from : true,to : false},{symbol: 'INR',from : false,to : true},
+    {symbol: 'USD',from : false,to : false},{symbol: 'GBP',from : false,to : false},
+    {symbol: 'AUD',from : false,to : false},{symbol: 'BRL',from : false,to : false},
+    {symbol: 'CNY',from : false,to : false},{symbol: 'AED',from : false,to : false}].sort();
+    this.calculate();
   }
 
   ngOnInit(): void {
@@ -38,13 +44,42 @@ export class ExchangeRateComponent implements OnInit,OnChanges {
       let property= properties[0];      
       this.exchangeRate = res.rates[property];
       console.log(this.fromValue,' to ',this.toValue,' exchange rate fetched = ',this.exchangeRate);
+      this.calculate();
     })},10000);
   }
 
   ngOnChanges() : void {
+    this.calculate();
+  }
+
+  calculate(){
     let fromVal = Number(this.fromCurrencyAmount);
     let toVal = fromVal*this.exchangeRate;
     console.log('converted = ',toVal);
     this.toCurrencyAmount = toVal+'';    
+  }
+
+  onFromCurrencySelection(event : any) {
+    console.log(event.target.value);
+    this.fromValue=event.target.value;
+    this.exchangeRateService.getExchangeRate(this.fromValue,this.toValue).subscribe((res : any) => {    // fetch latest exchange rate from the API  
+      let properties = Object.getOwnPropertyNames(res.rates);            
+      let property= properties[0];      
+      this.exchangeRate = res.rates[property];
+      console.log(this.fromValue,' to ',this.toValue,' exchange rate fetched = ',this.exchangeRate);
+      this.calculate();
+    });
+  }
+
+  onToCurrencySelection(event : any) {
+    console.log(event.target.value);
+    this.toValue=event.target.value;
+    this.exchangeRateService.getExchangeRate(this.fromValue,this.toValue).subscribe((res : any) => {    // fetch latest exchange rate from the API  
+      let properties = Object.getOwnPropertyNames(res.rates);            
+      let property= properties[0];      
+      this.exchangeRate = res.rates[property];
+      console.log(this.fromValue,' to ',this.toValue,' exchange rate fetched = ',this.exchangeRate);
+      this.calculate();
+    });
   }
 }
---
+++ discount-calculator-web/src/app/model/currency-model.ts
@@ -0,0 +1,5 @@
+export class CurrencyModel {
+    symbol!: string;
+    from! : boolean;
+    to! : boolean;
+}
